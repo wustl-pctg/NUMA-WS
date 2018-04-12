5aef3a47282e2001531e312de373aee11c0b316f
Justin Deters <j.deters@wustl.edu>
2018-04-12 12:23:41 -0500

    Small fix to file

--- runtime/sched_stats.h
+++ runtime/sched_stats.h
@@ -5,29 +5,31 @@
 #ifdef SCHED_STATS
 #define NUMBER_OF_STATS 3
 
+__CILKRTS_BEGIN_EXTERN_C
+
 enum timing
 {
-        INTERVAL_SCHED, //Scheduler Overhead
-            INTERVAL_WORK_INFLATION, //Work inflation measurement
-                INTERVAL_IDLE, // Idle time
+    INTERVAL_SCHED, //Scheduler Overhead
+    INTERVAL_WORK_INFLATION, //Work inflation measurement
+    INTERVAL_IDLE, // Idle time
 };
 
 typedef struct sched_stats
 {
-        volatile uint64_t time[NUMBER_OF_STATS]; // Total time measured for all stats
+    volatile uint64_t time[NUMBER_OF_STATS]; // Total time measured for all stats
 
-            volatile uint64_t begin[NUMBER_OF_STATS]; // Begin time of current measurement
+    volatile uint64_t begin[NUMBER_OF_STATS]; // Begin time of current measurement
 
-                volatile uint64_t end[NUMBER_OF_STATS];  // End time of current measurement
+    volatile uint64_t end[NUMBER_OF_STATS];  // End time of current measurement
 } stats;
 
 typedef struct total_stats
 {
-        double time[NUMBER_OF_STATS]; // Total time measured for all stats
+    double time[NUMBER_OF_STATS]; // Total time measured for all stats
 
-            double begin[NUMBER_OF_STATS]; // Begin time of current measurement
+    double begin[NUMBER_OF_STATS]; // Begin time of current measurement
 
-                double end[NUMBER_OF_STATS];  // End time of current measurement
+    double end[NUMBER_OF_STATS];  // End time of current measurement
 
 } total;
 
@@ -52,10 +54,9 @@ void __cilkrts_stop_timing(__cilkrts_worker *w, enum timing i);
 # define START_TIMING(w, i) __cilkrts_start_timing(w, i);
 # define STOP_TIMING(w, i) __cilkrts_stop_timing(w, i);
 # define DROP_TIMING(w, i) __cilkrts_drop_timing(w, i);
-
+__CILKRTS_END_EXTERN_C
 #else
 # define START_TIMING(w, i)
 # define STOP_TIMING(w, i)
 # define DROP_TIMING(w, i)
-
 #endif
